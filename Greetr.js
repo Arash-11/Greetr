7db9e7f85619b1a015e6e9ed1245ba1127024b87
Arash-11 <arashnawy@gmail.com>
2021-07-24 15:53:33 -0400

    added jquery support

--- Greetr.js
+++ Greetr.js
@@ -1,10 +1,12 @@
 // Create a new execution context for our library
-(function(global, $) {
+;(function(global, $) {
 
+  // 'new' an object
   var Greetr = function(firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   }
 
+  // hidden within the scope of the IIFE and never directly accessible
   var supportedLangs = ['en', 'es'];
 
   var greetings = {
@@ -72,15 +74,37 @@
       this.validate();
 
       return this;
+    },
+
+    HTMLGreeting: function(selector, formal) {
+      if (!$) throw 'jQuery not loaded';
+
+      if (!selector) throw 'Missing jQuery selector';
+
+      var msg;
+
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+
+      $(selector).html(msg);
+
+      return this;
     }
   };
 
   // Note: it's okay to set `Greetr.init` after `Greetr` because by the time `Greetr` will be called,
   // all the other code (eg `Greetr.init`) will be set up and run.
+
+  // the actual object is created here, allowing us to 'new' an object without calling 'new'
   Greetr.init = function(firstName, lastName, language) {
     this.firstName = firstName || '';
     this.lastName = lastName || '';
     this.language = language || 'en';
+
+    this.validate();
   }
 
   // any objects created with `Greetr.init` should point to `Greetr` as their prototype
--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('John', 'Doe');
-g.greet().setLang('fr').greet(true);
+
+g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function() {
+  // create a new 'Greetr' object (let's pretend we know the name from the login)
+  var loginGrtr = G$('John', 'Doe');
+
+  $('#logindiv').hide();
+
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
