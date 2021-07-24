7b348a4b68d6f0b5c7bec90b4a07cf8e8fd8dc93
Arash-11 <arashnawy@gmail.com>
2021-07-24 15:23:38 -0400

    set prototype methods

--- Greetr.js
+++ Greetr.js
@@ -5,7 +5,75 @@
     return new Greetr.init(firstName, lastName, language);
   }
 
-  Greetr.prototype = {};
+  var supportedLangs = ['en', 'es'];
+
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inicio sesion'
+  };
+
+  Greetr.prototype = {
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    validate: function() {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw "Invalid language";
+      }
+    },
+
+    greeting: function() {
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+
+    greet: function(formal) {
+      var msg;
+
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+
+      // IE (likely old versions) don't have a console so we need to add a check here
+      if (console) console.log(msg);
+
+      // 'this' refers to the calling object at execution time.
+      // Makes the method chainable
+      return this;
+    },
+
+    log: function() {
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+
+      return this;
+    },
+
+    setLang: function(lang) {
+      this.language = lang;
+
+      this.validate();
+
+      return this;
+    }
+  };
 
   // Note: it's okay to set `Greetr.init` after `Greetr` because by the time `Greetr` will be called,
   // all the other code (eg `Greetr.init`) will be set up and run.
